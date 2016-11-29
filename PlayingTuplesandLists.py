9851ae19895eaa1606b8472cccc01670831f2786
Axelsson Henrik (Consultant) <henrik.axelsson@consultant.volvo.com>
2016-11-29 12:35:04 +0100

    Adding slice and sort

--- PlayingTuplesandLists.py
+++ PlayingTuplesandLists.py
@@ -1,9 +1,14 @@
 
 x = [5,2,3,6,8,9,6,4,3,2,6,7]
+y = ['Nong', 'Mango', 'Bob']
+print (x.sort())
+print (x)
 
-print (x)
-x.insert(2,99)
-print (x)
+y.sort()
+print (y)
+
+# Slice
+print (x[5:7])
 
 
 
