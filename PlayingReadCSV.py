4a4b14193e744a1a6f10e64aedb3f371d01ff1a9
Axelsson Henrik (Consultant) <henrik.axelsson@consultant.volvo.com>
2016-11-29 13:54:34 +0100

    Working with index

--- PlayingReadCSV.py
+++ PlayingReadCSV.py
@@ -5,12 +5,25 @@ with open('aCSVFile.csv') as csvfile:
 
     latitudes = []
     longitudes = []
+    times = []
 
     for row in readCSV:
+        time = row[1]
         latitude = row[2]
         longitude = row[3]
+        times.append(time)
         latitudes.append(latitude)
         longitudes.append(longitude)
 
 print (latitudes)
 print (longitudes)
+
+whatTime = input ('What time index do you want to have lat/long for?')
+timeindex = times.index(whatTime)
+
+print (timeindex)
+
+theLat = latitudes[timeindex]
+theLong = longitudes[timeindex]
+
+print (theLat, theLong)
