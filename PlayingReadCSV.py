6c67049b352c2d916383cb728532d86b62e34a1f
Axelsson Henrik (Consultant) <henrik.axelsson@consultant.volvo.com>
2016-11-29 13:48:00 +0100

    Latitude and longitude added

--- PlayingReadCSV.py
+++ PlayingReadCSV.py
@@ -2,6 +2,15 @@ import csv
 
 with open('aCSVFile.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter = ',')
+
+    latitudes = []
+    longitudes = []
+
     for row in readCSV:
-        print (row[0])
-        print (row[0], row[1])
+        latitude = row[2]
+        longitude = row[3]
+        latitudes.append(latitude)
+        longitudes.append(longitude)
+
+print (latitudes)
+print (longitudes)
