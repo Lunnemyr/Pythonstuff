ec919562cbb24a34240cccd99d636ca47dec943b
Axelsson Henrik (Consultant) <henrik.axelsson@consultant.volvo.com>
2016-11-30 12:50:23 +0100

    Dictionaries again

--- PlayingDictionaries.py
+++ PlayingDictionaries.py
@@ -1,6 +1,19 @@
 # Keys and values
 # No order in a dictionary
 
-exDict = {'Jack':15, 'Bob':22, 'Alice':12, 'Kevin':17}
+exDict = {'Jack':[15, 'blonde'], 'Bob':[22,'brown'], 'Alice':[12,'black'], 'Kevin':[17, 'red']}
 
 print (exDict)
+print (exDict['Jack'])
+
+exDict['Tim'] = 14
+print (exDict)
+
+exDict['Tim'] = 15
+print (exDict)
+
+del exDict['Tim']
+print('Tim died')
+print (exDict)
+
+print (exDict['Jack'][1])
