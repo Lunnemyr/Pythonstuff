fff5a8a117381ddd11924effc38fff51d1a0c577
Axelsson Henrik (Consultant) <henrik.axelsson@consultant.volvo.com>
2016-12-02 10:24:56 +0100

    Using arguments in functions in Python

--- PlayingSystemModule.py
+++ PlayingSystemModule.py
@@ -1,11 +1,14 @@
 #System Module
 import sys
-sys.stderr.write('This is stderr text\n')
-sys.stderr.flush
-sys.stdout.write('This is std out text\n')
+#sys.stderr.write('This is stderr text\n')
+#sys.stderr.flush
+#sys.stdout.write('This is std out text\n')
 
 
 #Can pass arguments when calling this file, from command line or some other place
-if len(sys.argv) > 1:
-    print(float (sys.argv[1])+5)
     
+def main(arg):
+    print(arg)
+
+
+main(sys.argv[1])
